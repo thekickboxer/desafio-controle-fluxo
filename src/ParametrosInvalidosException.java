72625b2fc7f10937df1b3640bbb303c48366d5c2
Leandro de Souza Cruz Ferreira <leo_kickboxer@hotmail.com>
2023-09-22 16:57:10 -0300

    Projeto inicial

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException() {
+        super();
+    }
+    
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+}
